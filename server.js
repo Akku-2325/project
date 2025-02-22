c0940113839e18fff136f882a9b0b3d66958514f
Akku-2325 <231696@astanait.edu.kz>
2025-02-22 21:42:28 +0500

    Order

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-// const cors = require('cors');
-// const authRoutes = require('./routes/authRoutes');
-// const userRoutes = require('./routes/userRoutes');
-// const productRoutes = require('./routes/productRoutes');
-// const orderRoutes = require('./routes/orderRoutes');
+const cors = require('cors'); // Import the cors middleware
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes'); // Добавьте эту строку
 const errorHandler = require('./middleware/errorHandler');
 
 dotenv.config();
@@ -13,30 +13,30 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log("MONGODB_URI:", process.env.MONGODB_URI);
+console.log("MONGODB_URI:", process.env.MONGODB_URI); // Добавляем эту строку
 
-// app.use(cors());
-// app.use(express.json());
+// Enable CORS for all routes
+app.use(cors());
+
+// Middleware
+app.use(express.json());
 
 // Routes
-// app.use('/auth', authRoutes);
-// app.use('/users', userRoutes);
-// app.use('/products', productRoutes);
-// app.use('/orders', orderRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 // Error Handler
 app.use(errorHandler);
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
-
-app.get('/', (req, res) => {
-    res.send('Hello from Render!');
-});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
